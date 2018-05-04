54f4a02e524749ed041289b06a98c2cdff298ffe
Jay <janellguzman@gmail.com>
2018-05-03 17:17:22 -0700

    Finalizing files.

--- liri.js
+++ liri.js
@@ -1,136 +1,136 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-
 var request = require("request");
 var fs = require("fs");
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
 var action = process.argv[2];
 var value = process.argv.slice(3);
 
 switch (action) {
-  case "my-tweets":
-    getTwitter();
-    break;
+    case "my-tweets":
+        getTwitter();
+        break;
 
-  case "spotify-this-song":
-    if (process.argv.length < 4) {
-      value = "Mine"
-    }
-    getSpotify(value);
-    break;
+    case "spotify-this-song":
+        if (process.argv.length < 4) {
+            value = "The Girl is Mine"
+        }
+        getSpotify(value);
+        break;
 
-  case "movie-this":
-    if (process.argv.length < 4) {
-      value = "Mr Nobody"
-    }
-    getMovie();
-    break;
+    case "movie-this":
+        if (process.argv.length < 4) {
+            value = "Mr. Nobody"
+        }
+        getMovie();
+        break;
 
-  case "do-what-it-says":
-    doIt();
-    break;
+    case "do-what-it-says":
+        doIt();
+        break;
 
 }
 
 function getTwitter() {
-  var params = { screen_name: 'JGuz', count: 20 };
-  client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-      for (var i = 0; i < tweets.length; i++) {
-        var tweetInfo = (
-          "-----------------------------------------" +
-          "\n @JGuz: " + tweets[i].text + "\n Created At: " + tweets[i].created_at +
-          "\n---------------------------------------");
-        console.log(tweetInfo);
+    var params = { screen_name: 'JGuz56756996', count: 20 };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var tweetInfo = (
+                    "-----------------------------------------" +
+                    "\n @JGuz: " + tweets[i].text + "\n Created At: " + tweets[i].created_at +
+                    "\n---------------------------------------");
+                console.log(tweetInfo);
 
-        fs.appendFile("twitterLog.txt", tweetInfo, function(err) {
-          if (err) {
-            console.log(err);
-          }
-        
-        });
+                fs.appendFile("twitterLog.txt", tweetInfo, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
 
-      }
+                });
 
-    }
-  });
+            }
+
+        }
+    });
 }
 
 function getSpotify(value) {
-  spotify.search({ type: 'track', query: value }, function (err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
+    spotify.search({ type: 'track', query: value }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-    var song = data.tracks.items[0]
-    var songInfo = (
-      "--------------------" +
-      "\r\n Artist(s): " + song.artists[0].name +
-      "\r\n Song Title: " + song.name +
-      "\r\n Preview Song: " + song.preview_url +
-      "\r\n Album: " + song.album.name +
-      "\r\n--------------------");
-    console.log(songInfo);
+        var song = data.tracks.items[0]
+        var songInfo = (
+            "--------------------" +
+            "\r\n Artist(s): " + song.artists[0].name +
+            "\r\n Song Title: " + song.name +
+            "\r\n Preview Song: " + song.preview_url +
+            "\r\n Album: " + song.album.name +
+            "\r\n--------------------");
+        console.log(songInfo);
 
-    fs.appendFile("spotifyLog.txt", songInfo, function(err) {
+        fs.appendFile("spotifyLog.txt", songInfo, function (err) {
 
-      if (err) {
-        console.log(err);
-      }
-    
-      else {
-        console.log("Content Added to Text File!");
-      }
-    
-    });
-  })
+            if (err) {
+                console.log(err);
+            }
+
+            else {
+                console.log("Content Added to Text File!");
+            }
+
+        });
+    })
 }
 
 function getMovie() {
 
-  request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
-    if (!error && response.statusCode === 200) {
-      
-      var movieInfo = (
-        "--------------------" +
-        "\n Movie Title: " + JSON.parse(body).Title +
-        "\n Year: " + JSON.parse(body).Year +
-        "\n IMDB Rating: " + JSON.parse(body).imdbRating +
-        "\n Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
-        "\n Produced In: " + JSON.parse(body).Country +
-        "\n Language: " + JSON.parse(body).Language +
-        "\n Plot: " + JSON.parse(body).Plot +
-        "\n Actors: " + JSON.parse(body).Actors +
-        "\n--------------------");
-      console.log(movieInfo);
+        if (!error && response.statusCode === 200) {
 
-      fs.appendFile("omdbLog.txt", movieInfo, function(err) {
+            var movieInfo = (
+                "--------------------" +
+                "\n Movie Title: " + JSON.parse(body).Title +
+                "\n Year: " + JSON.parse(body).Year +
+                "\n IMDB Rating: " + JSON.parse(body).imdbRating +
+                "\n Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
+                "\n Produced In: " + JSON.parse(body).Country +
+                "\n Language: " + JSON.parse(body).Language +
+                "\n Plot: " + JSON.parse(body).Plot +
+                "\n Actors: " + JSON.parse(body).Actors +
+                "\n--------------------");
+            console.log(movieInfo);
 
-        if (err) {
-          console.log(err);
+            fs.appendFile("omdbLog.txt", movieInfo, function (err) {
+
+                if (err) {
+                    console.log(err);
+                }
+
+                else {
+                    console.log("Content added to txt file.");
+                }
+
+            });
+
         }
-      
-        else {
-          console.log("Content added to txt file.");
-        }
-      
-      });
 
-    }
-
-  });
+    });
 }
 
 function doIt() {
-  fs.readFile('random.txt', "utf8", function (error, data) {
-    var txt = data.split(',');
+    fs.readFile('random.txt', "utf8", function (error, data) {
+        var txt = data.split(',');
 
-    getSpotify(txt[1]);
-  });
+        getSpotify(txt[1]);
+    });
 }
